dfe68439f8f6a23d3d3e45c9623f2064383a2f68
= <hristijan1veleski@gmail.com>
2024-01-14 19:45:42 +0100

    Delete packer_compiled|nvim_treesitter_context

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,28 +1,28 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
 lsp.setup()
 
 -- You need to setup `cmp` after lsp-zero
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
 cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+	mapping = {
+		-- `Enter` key to confirm completion
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-    -- Ctrl+Space to trigger completion menu (Ubuntu)
-    -- Ctrl+b to trigger completion menu (MacOS) 
-    ['<C-b>'] = cmp.mapping.complete(),
+		-- Ctrl+Space to trigger completion menu (Ubuntu)
+		-- Ctrl+t to trigger completion menu (MacOS)
+		["<C-Space>"] = cmp.mapping.complete(),
 
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-  }
+		-- Navigate between snippet placeholder
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
+	},
 })
