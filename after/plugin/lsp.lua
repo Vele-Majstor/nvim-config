2f0140bdb681370101ae3b14f97dfca0bf7eda1b
Hristijan Veleski <hristijan1veleski@gmail.com>
2024-01-24 10:31:16 +0100

    Add new plugins | Cleanup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,17 +12,25 @@ lsp.setup()
 local cmp = require("cmp")
 local cmp_action = require("lsp-zero").cmp_action()
 
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load({ paths =  "~/.config/nvim/snippets"  })
+
 cmp.setup({
 	mapping = {
-		-- `Enter` key to confirm completion
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
-
-		-- Ctrl+Space to trigger completion menu (Ubuntu)
-		-- Ctrl+t to trigger completion menu (MacOS)
 		["<C-Space>"] = cmp.mapping.complete(),
-
-		-- Navigate between snippet placeholder
 		["<C-f>"] = cmp_action.luasnip_jump_forward(),
 		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 	},
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	}, {
+		{ name = 'buffer' },
+	}),
 })
--- lua/anna_couroff/packer.lua
+++ lua/anna_couroff/packer.lua
@@ -2,13 +2,13 @@ vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
+	use("alec-gibson/nvim-tetris")
 
 	use({
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.2",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
-
 	use("folke/tokyonight.nvim")
 	use({
 		"nvim-treesitter/nvim-treesitter",
@@ -31,9 +31,25 @@ return require("packer").startup(function(use)
 
 			{ "hrsh7th/nvim-cmp" },
 			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "L3MON4D3/LuaSnip" },
 		},
 	})
 	use("jose-elias-alvarez/null-ls.nvim")
 	use("MunifTanjim/prettier.nvim")
+	use({ "catppuccin/nvim", as = "catppuccin" })
+
+	use({
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!:).
+		run = "make install_jsregexp",
+	})
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
 end)
